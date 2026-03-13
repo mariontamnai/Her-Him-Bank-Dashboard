b0cd9ce8c4cef1d5fa30c8b53452a03dac1b7b85
Marion <mariontamnai@gmail.com>
2026-03-13 13:06:08 +0300

    DEV-5: Fix environment variables for Supabase keys

--- src/app/features/auth/login/supabase.ts
+++ src/app/features/auth/login/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { environment } from '../../../../environments/environment';
 
-const supabaseUrl = 'https://wzhqfnnjzyovmbbqmeuk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6aHFmbm5qenlvdm1iYnFtZXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDAyODEsImV4cCI6MjA4ODkxNjI4MX0.cSsZJy99846xWeCu9qHS2NMRqRKxrKdj91cV22lYw4U';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  environment.supabaseUrl,
+  environment.supabaseKey
+);
